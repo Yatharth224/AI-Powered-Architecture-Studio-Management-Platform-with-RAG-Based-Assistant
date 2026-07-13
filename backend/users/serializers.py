6c9a63d0587e6efb456d890da94ab5500d8199f4
Your Name <yatharthchouhan22@gmail.com>
2026-07-13 23:45:33 +0530

    user serializers code add

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,1 +1,14 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from  .models import ArchitectInvite
+
+
+User = get_user_model()
+
+class ArchitectInviteSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(write_only=True,min_length=8)
+    confirm_password = serializers.CharField(write_only=True)
+
+
+    
