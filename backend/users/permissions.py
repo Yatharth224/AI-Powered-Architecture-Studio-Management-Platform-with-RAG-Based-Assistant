74681db87c4de58c21e048623b5b2e5377104d29
Your Name <yatharthchouhan22@gmail.com>
2026-07-14 23:11:22 +0530

    user permission is client permission

--- backend/users/permissions.py
+++ backend/users/permissions.py
@@ -4,3 +4,13 @@ class IsAdminUser(BasePermission):
 
     def has_permission(self, request, view):
         return request.user and request.user.is_authenticated and request.user.role == 'admin'
+    
+
+class IsClient(BasePermission):
+    
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated and
+            request.user.role == 'client'
+        )
+
