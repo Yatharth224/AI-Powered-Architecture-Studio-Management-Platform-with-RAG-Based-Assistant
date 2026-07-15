da49614dbe5e01d7ebf187e2b84316b7434f1aa2
Your Name <yatharthchouhan22@gmail.com>
2026-07-15 23:35:14 +0530

    user views all imports

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+import logging
+from datetime import timedelta
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from .models import ArchitectInvite
+from .permissions import IsAdmin
+from .serializers import (
+    ClientRegisterSerializer,
+    UserProfileSerializer,
+    UserListSerializer,
+    UpdateRoleSerializer,
+    ArchitectInviteSerializer,
+    ArchitectRegisterSerializer,
+)
