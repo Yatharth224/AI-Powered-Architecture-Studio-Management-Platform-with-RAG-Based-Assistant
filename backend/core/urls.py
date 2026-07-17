d3f85f6e1b773ef05740565f8a7a05b96a6374a2
Your Name <yatharthchouhan22@gmail.com>
2026-07-17 23:32:56 +0530

    core urls updated

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -22,10 +22,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/auth/', include('users.urls')),
+    path('admin/',        admin.site.urls),
+    path('api/auth/',     include('users.urls')),
+    path('api/projects/', include('projects.urls')),
+    path('api/documents/', include('documents.urls')),  
 ]
-
 if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL,
