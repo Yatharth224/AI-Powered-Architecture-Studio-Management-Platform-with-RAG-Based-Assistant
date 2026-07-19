3c8d3d476ff06f736085e4b3ef4d72b43249e1fd
Your Name <yatharthchouhan22@gmail.com>
2026-07-19 11:22:38 +0530

    documents urls

---
+++ backend/documents/urls.py
@@ -0,0 +1,9 @@
+# documents/urls.py
+
+from django.urls import path
+from .views import DocumentListView, DocumentDetailView
+
+urlpatterns = [
+    path('', DocumentListView.as_view(), name='document_list'),
+    path('<int:pk>/', DocumentDetailView.as_view(), name='document_detail'),
+]
