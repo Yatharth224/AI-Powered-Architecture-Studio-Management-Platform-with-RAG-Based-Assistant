4957f01496dbc59807f62ae7b9a07623ab8bf9e7
Your Name <yatharthchouhan22@gmail.com>
2026-07-24 23:09:33 +0530

    analytics views import

--- backend/analytics/views.py
+++ backend/analytics/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.db.models import Count, Sum, Q
+from django.utils import timezone
+from datetime import timedelta
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from users.permissions import IsAdmin
+from projects.models import Project, Milestone
+from inquiries.models import Inquiry
+from users.models import User
