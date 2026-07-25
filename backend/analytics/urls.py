61811e1187e458335045c09e77c9d109026d8e0a
Your Name <yatharthchouhan22@gmail.com>
2026-07-25 23:20:42 +0530

    analytics urls first url pattern

--- backend/analytics/urls.py
+++ backend/analytics/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from .views import DashboardStatsView, ProjectAnalyticsView
 
 urlpatterns = [
-    path('dashboard/', DashboardStatsView.as_view(), name='dashboard_stats'),]
-    
+    path('dashboard/', DashboardStatsView.as_view(), name='dashboard_stats'),
+    path('projects/',  ProjectAnalyticsView.as_view(), name='project_analytics'),
+]
